b8dee8e60d2ba8121178bee4bdabdaf95c667243
LuisHenriique <luishenrique162004@gmail.com>
2022-05-12 20:15:40 -0300

    menu-mobile e acessibilidade da página

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
+const body = document.querySelector('body')
 const nav = document.querySelector('#navagation')
+const openMenu = document.querySelector('.openMenu')
+const closeMenu = document.querySelector('.closeMenu')
 
 document.addEventListener('scroll', function onScroll() {
   const positionY = scrollY
   positionY !== 0 ? nav.classList.add('scroll') : nav.classList.remove('scroll')
+})
+
+openMenu.addEventListener('click', function (e) {
+  body.classList.add('menu-expanded')
+})
+
+closeMenu.addEventListener('click', function (e) {
+  body.classList.remove('menu-expanded')
 })
