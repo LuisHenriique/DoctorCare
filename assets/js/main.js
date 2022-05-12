36084cf7f27e09cf0fc5ebb368ee8cd6b021a432
LuisHenriique <luishenrique162004@gmail.com>
2022-05-11 22:23:37 -0300

    Criando menu Javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log()
+const nav = document.querySelector('nav')
+function onScroll() {
+  const positionY = scrollY
+  if (positionY !== 0) {
+    nav.classList.add('scroll')
+  } else {
+    nav.classList.remove('scroll')
+  }
+}
+
+document.addEventListener('scroll', onScroll())
